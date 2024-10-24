58145aa3f74f380635b7f06e96fdb903eb25c488
Leonardo Santos <leonardojosantos@gmail.com>
2024-10-23 23:06:46 -0300

    refatoração

--- 02_tarefas/03_construcao_de_apis/src/crud/animal-crud.js
+++ 02_tarefas/03_construcao_de_apis/src/crud/animal-crud.js
@@ -11,52 +11,67 @@ import { loadAnimal } from '../utils/load-animal.js';
 )();
 
 const createAnimal = async (request, response) => {
-    const {
-        animal_name,
-        animal_type,
-        animal_color,
-        animal_breed,
-        animal_description,
-    } = request.body;
+    const bodyData = {
+        ...request.body
+    };
 
-    const animal = {
-        animal_id: Date.now(),
-        animal_name: String(animal_name).toLowerCase().trim(),
-        animal_type: String(animal_type).toLowerCase().trim(),
-        animal_color: String(animal_color).toLowerCase().trim(),
-        animal_breed: String(animal_breed).toLowerCase().trim(),
-        animal_description: String(animal_description).toLowerCase().trim(),
+    const updatedData = {
+        animal_id: Date
+            .now(),
+
+        animal_name: String(bodyData.animal_name)
+            .toLowerCase()
+            .trim(),
+
+        animal_type: String(bodyData.animal_type)
+            .toLowerCase()
+            .trim(),
+
+        animal_color: String(bodyData.animal_color)
+            .toLowerCase()
+            .trim(),
+
+        animal_breed: String(bodyData.animal_breed)
+            .toLowerCase()
+            .trim(),
+
+        animal_description: String(bodyData.animal_description)
+            .toLowerCase()
+            .trim(),
     };
 
     const data = await loadAnimal();
 
-    data.push(animal);
+    data.push(updatedData);
 
     await fs.writeFile(
-        absoluteFilePath, formatJsonFile(data)
+        absoluteFilePath,
+        formatJsonFile(data)
     );
 
-    return response.status(201).send(
-        { message: 'Registro criado com sucesso!', animal }
-    );
+    return response
+        .status(201)
+        .send({ message: 'Registro criado com sucesso!', updatedData });
 };
 
 const readAllAnimal = async (request, response) => {
     const data = await loadAnimal();
 
     if (data.length === 0) {
-        return response.status(404).send(
-            { message: 'Nenhum registro cadastrado!' }
-        );
+        return response
+            .status(404)
+            .send({ message: 'Nenhum registro cadastrado!' });
     }
 
-    return response.status(200).send(
-        { message: 'Registros cadastrados', data }
-    );
+    return response
+        .status(200)
+        .send({ message: 'Registros cadastrados', data });
 }
 
 const readAnimalById = async (request, response) => {
-    const id = Number(request.params.id);
+    const id = Number(
+        request.params.id
+    );
 
     const data = await loadAnimal();
 
@@ -68,20 +83,48 @@ const readAnimalById = async (request, response) => {
         const item = data.find(
             (item) => item.animal_id === id
         );
-        return response.status(200).send(
-            { message: 'Dados do registro', item }
-        );
+
+        return response
+            .status(200)
+            .send({ message: 'Dados do registro', item });
     }
-    else {
-        return response.status(404).send(
-            { message: `Registro com id ${id} não encontrado!` }
-        );
-    }
+
+    return response
+        .status(404)
+        .send({ message: `Registro com id '${id}' não encontrado!` });
 }
 
 const updateAnimalById = async (request, response) => {
-    const id = Number(request.params.id);
-    const updatedData = request.body;
+    const id = Number(
+        request.params.id
+    );
+
+    const bodyData = {
+        ...request.body
+    };
+
+    const updatedData = {
+        animal_name: String(bodyData.animal_name)
+            .toLowerCase()
+            .trim(),
+
+        animal_type: String(bodyData.animal_type)
+            .toLowerCase()
+            .trim(),
+
+        animal_color: String(bodyData.animal_color)
+            .toLowerCase()
+            .trim(),
+
+        animal_breed: String(bodyData.animal_breed)
+            .toLowerCase()
+            .trim(),
+
+        animal_description: String(bodyData.animal_description)
+            .toLowerCase()
+            .trim(),
+    };
+
     const data = await loadAnimal();
 
     const index = data.findIndex(
@@ -95,22 +138,25 @@ const updateAnimalById = async (request, response) => {
         };
 
         await fs.writeFile(
-            absoluteFilePath, formatJsonFile(data)
+            absoluteFilePath,
+            formatJsonFile(data)
         );
 
-        return response.status(200).send(
-            { message: `Registro de id ${id} atualizado!` }
-        );
+        return response
+            .status(200)
+            .send({ message: `Registro de id '${id}' atualizado!` });
     }
-    else {
-        return response.status(404).send(
-            { message: `Registro de id ${id} não encontrado!` }
-        );
-    }
+
+    return response
+        .status(404)
+        .send({ message: `Registro de id '${id}' não encontrado!` });
 }
 
 const deleteAnimalById = async (request, response) => {
-    const id = Number(request.params.id);
+    const id = Number(
+        request.params.id
+    );
+
     const data = await loadAnimal();
 
     const filteredData = data.filter(
@@ -119,18 +165,18 @@ const deleteAnimalById = async (request, response) => {
 
     if (filteredData.length < data.length) {
         await fs.writeFile(
-            absoluteFilePath, formatJsonFile(filteredData)
+            absoluteFilePath,
+            formatJsonFile(filteredData)
         );
 
-        return response.status(200).send(
-            { message: `Registro de id ${id} excluído!` }
-        );
+        return response
+            .status(200)
+            .send({ message: `Registro de id '${id}' excluído!` });
     }
-    else {
-        return response.status(404).send(
-            { message: 'Registro não encontrado!' }
-        );
-    }
+
+    return response
+        .status(404)
+        .send({ message: `Registro de id '${id}'não encontrado!` });
 }
 
 export {
--- 02_tarefas/03_construcao_de_apis/src/utils/endpoints.js
+++ 02_tarefas/03_construcao_de_apis/src/utils/endpoints.js
@@ -1,7 +1,7 @@
 export const endpoints = {
-    createAnimal: String('/api/animais'),
-    readAllAnimals: String('/api/animais'),
-    readAnimalById: String('/api/animais/:id'),
-    updateAnimalById: String('/api/animais/:id'),
-    deleteAnimalById: String('/api/animais/:id'),
+    createAnimal: String('/api/animais/create-animal'),
+    readAllAnimals: String('/api/animais/read-all-animals'),
+    readAnimalById: String('/api/animais/read-animal/:id'),
+    updateAnimalById: String('/api/animais/update-animal/:id'),
+    deleteAnimalById: String('/api/animais/delete-animal/:id'),
 };
