4487df52f3b560302ac5d1c8ebbc5450cbc9a259
Leonardo Santos <leonardojosantos@gmail.com>
2024-11-08 13:25:23 -0300

    Banco de dados -- Aula - JSON: Dados em JavaScript

---
+++ 01_modulos/03_banco_de_dados/aula_json_dados_em_javascript/assets/js/index.js
@@ -0,0 +1,53 @@
+import {
+    criateObject,
+    displayInConsole,
+    stackElementArray,
+    tellMeHi,
+    transformsDataIntoJsObject,
+    transformsDataIntoJson,
+} from "../utils/functions.js";
+
+(() => {
+    const animal_cat = criateObject("gato", "o felino", 7, "07/07");
+    displayInConsole(animal_cat);
+
+    const animal_dog = criateObject("cachorro", "o canino", 12, "08/08");
+    displayInConsole(animal_dog);
+
+    const list_pets = [];
+    stackElementArray(list_pets, animal_dog, animal_cat);
+    displayInConsole(list_pets);
+
+    const list_in_json = transformsDataIntoJson(list_pets);
+    displayInConsole(list_in_json);
+    displayInConsole(transformsDataIntoJsObject(list_in_json));
+
+    const date_today = new Date();
+    displayInConsole(date_today);
+
+    const date_in_json = transformsDataIntoJson(date_today);
+    displayInConsole(date_in_json);
+
+    const return_date = transformsDataIntoJsObject(date_in_json);
+    displayInConsole(return_date);
+    displayInConsole(new Date(return_date));
+
+    animal_dog.pet_shop = date_today;
+    displayInConsole(animal_dog);
+
+    animal_cat.pet_shop = date_today;
+    displayInConsole(animal_cat);
+
+    const new_list = [];
+    stackElementArray(new_list, animal_dog, animal_cat);
+    displayInConsole(new_list);
+
+    const new_value_in_json = transformsDataIntoJson(new_list);
+    displayInConsole(new_value_in_json);
+
+    const json_wrong = '[{name: "Gabriel"}]';
+    tellMeHi(json_wrong);
+
+    const json_correct = '[{"name": "Gabriel"}]';
+    tellMeHi(json_correct);
+})();
---
+++ 01_modulos/03_banco_de_dados/aula_json_dados_em_javascript/assets/utils/functions.js
@@ -0,0 +1,39 @@
+export const criateObject = (type, name, age, birthdate) => {
+    return {
+        type: type,
+        name: name,
+        age: age,
+        birthdate: birthdate,
+    };
+};
+
+export const stackElementArray = (array, ...elementos) => {
+    array.push(elementos);
+};
+
+export const transformsDataIntoJson = (data) => {
+    return JSON.stringify(data);
+};
+
+export const transformsDataIntoJsObject = (data) => {
+    return JSON.parse(data);
+};
+
+export const tellMeHi = (data) => {
+    try {
+        transformsDataIntoJsObject(data);
+        displayInConsole("oi!");
+    }
+    catch (error) {
+        console.log("bloco try falhou -->", error);
+    }
+};
+
+export const displayInConsole = (argument_1, argument_2 = null) => {
+    if (argument_1) {
+        console.log(argument_1);
+    }
+    else if (argument_1 && argument_2) {
+        console.log(argument_1, argument_2);
+    }
+};
