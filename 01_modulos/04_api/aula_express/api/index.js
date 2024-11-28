4c261dd595d6876b6abd85010f24c5ef52cb05d1
Leonardo Santos <leonardojosantos@gmail.com>
2024-11-28 01:46:08 -0300

    API -- Aula - Express

---
+++ 01_modulos/04_api/aula_express/api/index.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3000;
+
+
+const utils = {
+    replaceOnListByIndex: (list, index, newData) => {
+        const listCopy = [...list];
+        listCopy[index] = newData;
+        return listCopy;
+    },
+};
+
+let profilesList = [];
+
+const endpoints = {
+    home: "/",
+    getProfileById: "/:id",
+    addProfile: "/add_profile",
+    removeProfileById: "/remove_profile/:id",
+    updateProfileById: "/update_profile/:id",
+};
+
+const statusCode = {
+    _200: 200,
+    _201: 201,
+    _404: 404,
+    _500: 500,
+};
+
+app.use(
+    cors()
+);
+
+// retornar perfis
+app.get(
+    endpoints.home, (req, res) => {
+        if (profilesList.length === 0) {
+            return res
+                .status(statusCode._200)
+                .send({ message: "Nenhum perfil registrado!" });
+        }
+        return res
+            .status(statusCode._200)
+            .send(profilesList);
+    }
+);
+
+// criar novo perfil
+app.post(
+    endpoints.addProfile, (req, res) => {
+        const profile = req.query;
+        profilesList.push(profile);
+
+        return res
+            .status(statusCode._201)
+            .send(profile);
+    }
+);
+
+// retornar perfil por id
+app.get(
+    endpoints.getProfileById, (req, res) => {
+        const { id } = req.params;
+
+        if (!profilesList[id]) {
+            return res
+                .status(statusCode._404)
+                .send({ message: `Nenhum registro encontrado com id '${id}'` });
+        }
+
+        return res
+            .status(statusCode._200)
+            .send(profilesList[id]);
+    }
+);
+
+// atualizar perfil por id
+app.put(
+    endpoints.updateProfileById, (req, res) => {
+        const { id } = req.params;
+        let userProfile = profilesList[id];
+
+        if (!userProfile) {
+            return res
+                .status(statusCode._404)
+                .send({ message: `Nenhum registro encontrado com id '${id}'` });
+        }
+
+        userProfile = {
+            ...userProfile,
+            ...req.query,
+        };
+
+        profilesList = utils.replaceOnListByIndex(profilesList, id, userProfile);
+
+        return res
+            .status(statusCode._200)
+            .send(userProfile);
+    }
+);
+
+// remover perfil por if
+app.delete(
+    endpoints.removeProfileById, (req, res) => {
+        const { id } = req.params;
+        let userProfile = profilesList[id];
+
+        if (!userProfile) {
+            return res
+                .status(statusCode._404)
+                .send({ message: "Not Found" });
+        }
+
+        profilesList = profilesList.filter(
+            (_item, index) => index !== Number(id)
+        );
+
+        return res
+            .status(statusCode._200)
+            .send(profilesList);
+    }
+);
+
+app.listen(
+    port, () => {
+        console.log(`Servidor executando em http://localhost:${port}`);
+    }
+);
