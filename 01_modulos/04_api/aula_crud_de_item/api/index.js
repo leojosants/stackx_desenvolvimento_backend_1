b2001f77cd571bfa1e3d83e460abc8441ce3fbc8
Leonardo Santos <leonardojosantos@gmail.com>
2024-11-28 10:31:01 -0300

    API -- Aula - CRUD de Item (finalizada)

--- 01_modulos/04_api/aula_crud_de_item/api/index.js
+++ 01_modulos/04_api/aula_crud_de_item/api/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const sqlit3 = require("sqlite3").verbose()
+const sqlit3 = require("sqlite3").verbose();
+
+
 const app = express();
 const port = 3000;
-
-
 const db = new sqlit3.Database("database/db.db");
 
 db.serialize(
@@ -19,16 +19,6 @@ db.serialize(
     }
 );
 
-let profilesList = [];
-
-const utils = {
-    replaceOnListByIndex: (list, index, newData) => {
-        const listCopy = [...list];
-        listCopy[index] = newData;
-        return listCopy;
-    },
-};
-
 const endpoints = {
     home: "/",
     getProfileById: "/:id",
@@ -38,9 +28,16 @@ const endpoints = {
 };
 
 const statusCode = {
+    // OK é uma resposta padrão que indica que uma solicitação HTTP foi bem-sucedida.
     _200: 200,
+
+    // Created é uma resposta que indica que uma solicitação foi bem-sucedida e resultou na criação de um novo recurso no servidor. 
     _201: 201,
+
+    // Not Found é uma resposta comum que indica que o servidor não conseguiu encontrar o recurso solicitado pelo cliente.
     _404: 404,
+
+    // Not Acceptable é uma resposta de erro do cliente que indica que o servidor não pode fornecer uma resposta que corresponda às características de conteúdo solicitadas pelo cliente, conforme especificado nos cabeçalhos da solicitação. 
     _406: 406,
 };
 
@@ -49,8 +46,7 @@ function getUserById(id) {
         (resolve, reject) => {
             db.get(
                 "SELECT * FROM profiles WHERE id = ?", [id], (err, row) => {
-                    console.log("row", row)
-                    row ? row : undefined;
+                    resolve(row ? row : undefined);
                 }
             );
         }
@@ -68,6 +64,12 @@ app.get(
             () => {
                 db.all(
                     "SELECT * FROM profiles", (err, rows) => {
+                        if (rows.length === 0) {
+                            return res
+                                .status(statusCode._200)
+                                .send({ message: "Não há dados registrados no momento!" });
+                        }
+
                         return res
                             .status(statusCode._200)
                             .send(rows);
@@ -93,7 +95,7 @@ app.post(
             () => {
                 db.run(`
                     INSERT INTO profiles(name, age) 
-                    VALUES('${name}', ${age ? `'${age}'` : "NULL"})
+                    VALUES('${name}', ${age ? `${Number(age)}` : "NULL"})
                 `);
 
                 db.all(
@@ -110,28 +112,9 @@ app.post(
 
 // retornar perfil por id
 app.get(
-    endpoints.getProfileById, (req, res) => {
+    endpoints.getProfileById, async (req, res) => {
         const { id } = req.params;
-        const userProfile = getUserById(id);
-        console.log(userProfile);
-
-        // if (!profilesList[id]) {
-        //     return res
-        //         .status(statusCode._404)
-        //         .send({ message: `Nenhum registro encontrado com id '${id}'` });
-        // }
-
-        return res
-            .status(statusCode._200)
-            .send({});
-    }
-);
-
-// atualizar perfil por id
-app.put(
-    endpoints.updateProfileById, (req, res) => {
-        const { id } = req.params;
-        let userProfile = profilesList[id];
+        const userProfile = await getUserById(id);
 
         if (!userProfile) {
             return res
@@ -139,38 +122,92 @@ app.put(
                 .send({ message: `Nenhum registro encontrado com id '${id}'` });
         }
 
-        userProfile = {
-            ...userProfile,
-            ...req.query,
-        };
-
-        profilesList = utils.replaceOnListByIndex(profilesList, id, userProfile);
-
         return res
             .status(statusCode._200)
             .send(userProfile);
     }
 );
 
-// remover perfil por if
-app.delete(
-    endpoints.removeProfileById, (req, res) => {
+// atualizar perfil por id
+app.put(
+    endpoints.updateProfileById, async (req, res) => {
         const { id } = req.params;
-        let userProfile = profilesList[id];
+        const { name, age } = req.query;
+        const userProfile = await getUserById(id);
 
         if (!userProfile) {
             return res
                 .status(statusCode._404)
-                .send({ message: "Not Found" });
-        }
+                .send({ message: `Nenhum registro encontrado com id '${id}'` });
+        };
 
-        profilesList = profilesList.filter(
-            (_item, index) => index !== Number(id)
+        if (!name) {
+            return res
+                .status(statusCode._406)
+                .send({ message: "O nome é obrigatório" });
+        };
+
+        if (!age) {
+            return res
+                .status(statusCode._406)
+                .send({ message: "A idade é obrigatório" });
+        };
+
+        db.serialize(
+            () => {
+                const attributes = new Array();
+
+                name && attributes.push(
+                    `name = '${name}'`
+                );
+
+                age && attributes.push(
+                    `age = ${Number(age)}`
+                );
+
+                db.run(
+                    `UPDATE profiles SET ${attributes.join(", ")} WHERE id = ${id}`
+                );
+
+                db.all(
+                    `SELECT * FROM profiles WHERE id = ${id}`, (err, row) => {
+                        return res
+                            .status(statusCode._200)
+                            .send(row);
+                    }
+                );
+            }
+        );
+    }
+);
+
+// remover perfil por if
+app.delete(
+    endpoints.removeProfileById, async (req, res) => {
+        const { id } = req.params;
+        const userProfile = await getUserById(id);
+
+        if (!userProfile) {
+            return res
+                .status(statusCode._404)
+                .send({ message: `Nenhum registro encontrado com id '${id}'` });
+        };
+
+        db.serialize(
+            () => {
+                db.run(
+                    `DELETE FROM profiles WHERE id = ${id}`
+                )
+            }
         );
 
-        return res
-            .status(statusCode._200)
-            .send(profilesList);
+        db.all(
+            "SELECT * FROM profiles", (err, rows) => {
+                return res
+                    .status(statusCode._200)
+                    .send(rows);
+            }
+        );
     }
 );
 
